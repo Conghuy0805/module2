3c54b5c029136c76493a79cc7ffbc87481c024d1
unknown <you@example.com>
2023-07-10 23:07:50 +0700

    nop

--- src/ss1_introduction_in_java/bai_tap/Hien_thi_loi_chao.java
+++ src/ss1_introduction_in_java/bai_tap/Hien_thi_loi_chao.java
@@ -2,7 +2,7 @@ package ss1_introduction_in_java.bai_tap;
 
 import java.util.Scanner;
 
-public class e1_hello {
+public class Hien_thi_loi_chao {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name");
